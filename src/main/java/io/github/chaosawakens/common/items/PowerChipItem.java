34f47d391fa934d263a89411316434a7783e0f6e
negacem925 <116527412+negacem925@users.noreply.github.com>
2022-10-24 01:10:05 +0100

    Delete src/main directory

--- src/main/java/io/github/chaosawakens/common/items/PowerChipItem.java
+++
@@ -1,9 +0,0 @@
-package io.github.chaosawakens.common.items;
-
-import net.minecraft.item.Item;
-
-public class PowerChipItem extends Item {
-	public PowerChipItem(Properties builderIn) {
-		super(builderIn);
-	}
-}
--- src/main/java/io/github/chaosawakens/common/registry/CABlockStateProperties.java
+++
@@ -1,7 +0,0 @@
-package io.github.chaosawakens.common.registry;
-
-import net.minecraft.state.IntegerProperty;
-
-public class CABlockStateProperties {
-	public static final IntegerProperty URANIUM_GLOW_STRENGTH = IntegerProperty.create("glow_strength", 0, 5);
-}
